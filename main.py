6c396eb6f9e0b6fa35a3df2935d1febb1daf105e
Scott Phillips <scott.phillips@earnin.com>
2021-04-26 10:52:35 -0400

    Clean sample config file

--- main.py
+++ main.py
@@ -13,7 +13,9 @@ import lightspeedconnection
 def create_function_map() -> Dict[str, Callable]:
     return {'syncshippo': sync_shippo,
             'downloadschedule': download_lightspeed_schedule,
-            'displayschedule': display_schedule_info
+            'displayschedule': display_schedule_info,
+            'inventoryspreadsheet': inventory_spreadsheet,
+            'democonfigupdate': update_sample_config
             }
 
 
@@ -71,6 +73,35 @@ def display_schedule_info() -> None:
     raise NotImplementedError
 
 
+def inventory_spreadsheet() -> None:
+    logging.info("Updating inventory spreadsheet from lightspeed")
+    lightspeed_config: Dict = parse_config()["lightspeed"]
+    google_config: Dict = parse_config()["googlesheets"]
+
+    raise NotImplementedError
+
+
+def update_sample_config() -> None:
+    config = parse_config()
+    secret_entries = ["smartetailing/baseurl", "smartetailing/merchant_id", "smartetailing/url_key",
+                      "shippo/apikey",
+                      "lightspeed/account_id", "lightspeed/client_id", "lightspeed/client_secret",
+                      "lightspeed/password", "lightspeed/userid",
+                      "lightspeed/token_info/access_token", "lightspeed/token_info/refresh_token"]
+
+    # Search through the dictionary and rewrite the secrets.
+    for secret_to_clean in secret_entries:
+        my_config = config
+        all_keys = secret_to_clean.split('/')
+        for key in all_keys[:-1]:
+            my_config = my_config[key]
+        my_config[all_keys[-1]] = '***SECRET***'
+
+    config_file = "sample_config.json"
+    with open(config_file, 'w') as f:
+        json.dump(config, f, sort_keys=True, indent=2)
+
+
 def main():
     dir_path: str = os.path.dirname(os.path.realpath(__file__))
     log_file = os.path.join(dir_path, 'lightspeedsync.log')
@@ -79,7 +110,7 @@ def main():
                         datefmt='%Y-%m-%d %H:%M:%S',
                         filemode='w',
                         level=logging.DEBUG)
-    logging.debug('Started')
+    logging.debug(f'Started argv={sys.argv}  path={os.getcwd()}')
     try:
         args = parse_arguments()
         func = create_function_map()[args.command]
